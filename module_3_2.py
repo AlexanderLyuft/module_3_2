e2731885bb7fd68bc9eeb0cec135deb9c375b5eb
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-08-10 07:58:44 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,24 +1,25 @@
 # Домашнее задание "Способы вызова функции"
 # Исправленый
 
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    if (("@" and (".com" or ".ru" or ".net")) not in (recipient and sender) and
-        ("@" and (".com" or ".ru" or ".net")) not in (recipient and sender)):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-    if recipient == sender:
-        print(f"Нельзя отправить письмо самому себе!")
-        return
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-        return
-    elif sender != "university.help@gmail.com":
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-        return
+def send_email(message, recipient,*, sender = "university.help@gmail.com"):
+    corr_recipient = '@' in recipient and (recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'))
+    corr_sender = '@' in sender and (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'))
+    if corr_recipient and corr_sender:
+        if recipient == sender:
+            print("Нельзя отправить письмо самому себе!")
+        else:
+            if sender == "university.help@gmail.com":
+                print(f'Письмо успешно отправлено с адреса {sender}, на адрес {recipient}.')
+            else:
+                print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender}, на адрес {recipient}.')
+    else:
+        print(f'Невозможно отправить письмо с адреса {sender}, на адрес {recipient}.')
+
+
 send_email('Это сообщение для проверки связи', 'alexanderlyuft@mail.ru')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
 
 
